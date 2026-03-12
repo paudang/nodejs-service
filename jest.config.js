87f9637c0aa072aea8334ccd3cbf2339a42b9f52
Phuc Dang <phucdangb1400718@gmail.com>
2026-03-12 09:24:16 +0700

    Update github ci

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,27 @@ module.exports = {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{js,ts}'],
-  testMatch: ['**/*.test.ts', '**/*.test.js'],
+  testMatch: ['**/*.test.ts', '**/*.test.js', '**/*.spec.ts', '**/*.spec.js'],
   preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   coveragePathIgnorePatterns: [
     "/node_modules/",
-    "/dist/"
-  ]
+    "/dist/",
+    "src/index",
+    "src/app",
+    "src/config/env",
+    "src/config/swagger",
+    "src/infrastructure/webserver/swagger",
+    "src/infrastructure/webserver/server"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
 };
